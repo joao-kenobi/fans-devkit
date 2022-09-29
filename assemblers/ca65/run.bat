1ec07839ab020edac167b1b930b8a14cc1fd885a
João Louvem <joao.louvem@outlook.com>
2022-09-29 11:09:05 -0300

    versionando

--- assemblers/ca65/run.bat
+++ assemblers/ca65/run.bat
@@ -3,6 +3,5 @@
 cls
 set emulator_path=..\..\emulators
 set source_code_path=..\..\output
-REM %emulator_path%\bsnes-plus-mdc\bsnes.exe %source_code_path%\rom.sfc
 
 %emulator_path%\Mesen-S\Mesen-S.exe %source_code_path%\rom.sfc
